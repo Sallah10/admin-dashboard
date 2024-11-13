1680de95c76bc21a8476e7416c967768cc85dbf9
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-11-14 00:35:56 +0100

    authentication added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,6 +6,8 @@ import { MenuIcon, ShieldCheckIcon, XIcon } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Fragment } from "react";
+import { signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
 const navigation = [
   { name: "Users", href: "/" },
@@ -16,7 +18,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export default function Navbar() {
+import { Session } from "next-auth";
+
+type Props = {
+  user: Session["user"];
+};
+
+
+export default function Navbar({user} : Props) {
   const pathname = usePathname();
 
   return (
@@ -51,10 +60,20 @@ export default function Navbar() {
               <div className="hidden sm:ml-6 sm:flex sm:items-center">
                 <Menu as="div" className="relative ml-3">
                   <div>
-                    <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
-                      <span className="sr-only">Open user menu</span>
-                      <Avvvatars value={"U"} />
-                    </Menu.Button>
+                  <Menu.Button className="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-slate-500 focus:ring-offset-2">
+                    <span className="sr-only">Open user menu</span>
+                    {user?.image ? (
+                      <Image
+                        className="h-8 w-8 rounded-full"
+                        src={user.image}
+                        height={32}
+                        width={32}
+                        alt={user?.name ?? 'avatar'}
+                      />
+                    ) : (
+                      <Avvvatars value={'U'} />
+                    )}
+                  </Menu.Button>
                   </div>
                   <Transition
                     as={Fragment}
@@ -66,7 +85,7 @@ export default function Navbar() {
                     leaveTo="transform opacity-0 scale-95"
                   >
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                      {false ? (
+                      {user ? (
                         <Menu.Item>
                           {({ active }) => (
                             <button
@@ -74,6 +93,7 @@ export default function Navbar() {
                                 active ? "bg-gray-100" : "",
                                 "flex w-full px-4 py-2 text-sm text-gray-700"
                               )}
+                              onClick={() => signOut()}
                             >
                               Sign out
                             </button>
@@ -81,17 +101,18 @@ export default function Navbar() {
                         </Menu.Item>
                       ) : (
                         <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={classNames(
-                                active ? "bg-gray-100" : "",
-                                "flex w-full px-4 py-2 text-sm text-gray-700"
-                              )}
-                            >
-                              Sign in
-                            </button>
-                          )}
-                        </Menu.Item>
+                        {({ active }) => (
+                          <button
+                            className={classNames(
+                              active ? "bg-gray-100" : "",
+                              "flex w-full px-4 py-2 text-sm text-gray-700"
+                            )}
+                            onClick={() => signIn("github")}
+                          >
+                            Sign in
+                          </button>
+                        )}
+                      </Menu.Item>
                       )}
                     </Menu.Items>
                   </Transition>
