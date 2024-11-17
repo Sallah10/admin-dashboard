5763475e4cf56ddf8ed1e04860a51cb19e7b576b
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-11-17 16:00:08 +0100

    prisma declaration added

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,6 +5,9 @@ declare global {
   var prisma: PrismaClient;
 }
 
+const prisma = new PrismaClient();
+export default prisma;
+
 const client = globalThis.prisma || new PrismaClient();
 if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
