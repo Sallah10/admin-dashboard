8e7b1559b49ec9acff250419209233e777efdace
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2024-12-02 11:43:41 +0100

    DB production test

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -48,7 +48,7 @@ export default function Navbar({user} : Props) {
                       className={classNames(
                         pathname === item.href
                           ? "border-slate-500 text-gray-900"
-                          : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
+                          : "border-t ransparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
                         "inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
                       )}
                       aria-current={pathname === item.href ? "page" : undefined}
