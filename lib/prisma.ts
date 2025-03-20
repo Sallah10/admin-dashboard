acb320bbebd52a47ac6bcdfe7f601d7e73be69ae
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-03-20 13:40:24 +0100

    refactor: simplify prisma import and update global declaration in page.tsx and prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,12 @@
-// 
-import { PrismaClient } from '@prisma/client';
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+declare global {
+  var prisma: PrismaClient;
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const client = globalThis.prisma || new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+
+export default client;
